31669ce424bc2aa1e7d7278e45026cf3a3a5f3ef
zaujulio <zauhdf@gmail.com>
2021-07-27 19:50:17 -0300

    :sparkles: Update VanillaJS with jest --init (jes.config.js and coverage/)

--- JS/Vanilla/src/index.test.js
+++ JS/Vanilla/src/index.test.js
@@ -3,3 +3,7 @@ const calc = require("../src/index.js");
 test("subtract 2 - 1 to equal 1", () => {
   expect(calc.sub(2, 1)).toBe(1);
 });
+
+test("add 2 + 1 to equal 3", () => {
+  expect(calc.add(2, 1)).toBe(3);
+});
