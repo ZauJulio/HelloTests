807b1679f279b4a9bf6792e842ee67536a0137af
zaujulio <zauhdf@gmail.com>
2021-07-28 01:38:15 -0300

    :sparkles: Add DOM Manipulation test for VanillaJS with Jest

--- JS/Vanilla/src/index.test.js
+++
@@ -1,9 +0,0 @@
-const calc = require("../src/index.js");
-
-test("subtract 2 - 1 to equal 1", () => {
-  expect(calc.sub(2, 1)).toBe(1);
-});
-
-test("add 2 + 1 to equal 3", () => {
-  expect(calc.add(2, 1)).toBe(3);
-});
--- JS/vanilla_ts/src/index.test.ts
+++ JS/vanilla_ts/src/index.test.ts
@@ -1,4 +1,4 @@
-import add from "./";
+import add from ".";
 
 test("add 2 + 1 to equal 3", () => {
   expect(add(2, 1)).toBe(3);
